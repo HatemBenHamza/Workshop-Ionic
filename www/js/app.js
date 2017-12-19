679179f8f4c1e6118d7dcee56e3018e02c5ec9fd
Hatem Ben Hamza <benhamzahatem1@gmail.com>
2017-12-19 20:33:04 +0100

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,49 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
+.controller('TodoCtrl', function($scope , $ionicPopup ,$ionicListDelegate) {
+  $scope.tasks =
+    [
+      {title: "Premiére", completed: false},
+      {title: "Deuxiéme", completed: false},
+      {title: "Troisiéme", completed: false},
+    ];
+
+    $scope.newTask = function() {
+      $ionicPopup.prompt({
+        title: "Nouvelle tâche",
+        template: "Entrer la tâche:",
+        inputPlaceholder: "Qu'avez-vous besoin de faire?",
+        okText: 'Soumettre',
+        cancelText : 'Annuler'
+      }).then(function(res) {    // promise 
+        if (res) $scope.tasks.push({title: res, completed: false});
+      })
+    };
+
+    $scope.edit = function(task) {
+      $scope.data = { response: task.title };
+      $ionicPopup.prompt({
+        title: "Edit Task",
+        scope: $scope
+      }).then(function(res) {    // promise 
+        if (res !== undefined) task.title = $scope.data.response;
+        $ionicListDelegate.closeOptionButtons()
+      })
+    };
+
+
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  
+  
+  $stateProvider
+      .state('details', {
+          url: '/Contenu',
+          templateUrl: '/contenu.html'
+      });
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
